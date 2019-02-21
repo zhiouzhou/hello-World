5ce0a97ec704005f17d108959f2b1f3657242a75
zhoulizhen <zhoulizhen@yijiupi.cn>
2019-02-21 10:28:38 +0800

    添加新项目

---
+++ src/common/HomeHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="homeHeader">
+      <p>易酒批头部</p>
+      <span>注销</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeHeader"
+    }
+</script>
+
+<style scoped lang="stylus">
+  #homeHeader{
+    height:120px
+    background: #ddd
+    padding:0 20px
+    display: flex
+    flex-direction:row
+    justify-content space-between
+    align-items center
+  }
+</style>
